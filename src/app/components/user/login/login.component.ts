6e96197f0df352c9b8d4183096ee907a07e31681
Trần Minh Hiếu <46083795+tranminhhieulkqn@users.noreply.github.com>
2021-05-04 11:56:14 +0700

    Update local storage service.

--- src/app/components/user/login/login.component.ts
+++ src/app/components/user/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
 import { AuthenticationService } from "../../../services/authentication.service";
+import { LocalStorageService } from "../../../services/local-storage.service";
 
 @Component({
   selector: 'app-login',
@@ -8,17 +10,26 @@ import { AuthenticationService } from "../../../services/authentication.service"
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private authenticationService: AuthenticationService) { }
+  constructor(
+    private authenticationService: AuthenticationService,
+    private localStorageService: LocalStorageService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
   }
 
-  login(formLogin: any){
+  login(formLogin: any) {
     this.authenticationService.login(formLogin.value)
-    .subscribe(
-      res => console.log(res),
-      err => console.log(err)
-    )
+      .subscribe(
+        res => {
+          console.log(res);
+          this.localStorageService.set("tokenUser", res.token);
+          this.localStorageService.setObject("infoUser", res.infoUser)
+          this.router.navigateByUrl("/");
+        },
+        err => console.log(err)
+      )
   }
 
 }
