e26b8c0fcd53b2e82347d5dfb3f83dabba807a86
Trần Minh Hiếu <46083795+tranminhhieulkqn@users.noreply.github.com>
2021-05-24 15:49:56 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const express = require('express');
 const app = express();
 
 // serve only the static files form the dist directory
-app.use(express.static('./dist/WebsiteInteriorDesign-Front-Angular'));
+app.use(express.static('./dist/websiteinteriordesign-front-angular'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/WebsiteInteriorDesign-Front-Angular/'}),
+    res.sendFile('index.html', {root: 'dist/websiteinteriordesign-front-angular/'}),
 );
 
 // start the app by listening on the default Heroku port
