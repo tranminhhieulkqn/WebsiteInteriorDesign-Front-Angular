dd36378395bddac7caea99a4c00d0975eda7cc3a
Trần Minh Hiếu <46083795+tranminhhieulkqn@users.noreply.github.com>
2021-07-15 00:29:01 +0700

    Update page edit post.

--- src/app/views/app/post/post.routing.ts
+++ src/app/views/app/post/post.routing.ts
@@ -5,6 +5,7 @@ import { PostListComponent } from './post-list/post-list.component';
 import { PostDetailComponent } from './post-detail/post-detail.component';
 import { PostManageComponent } from './post-manage/post-manage.component';
 import { PostCreateComponent } from './post-create/post-create.component';
+import { PostEditComponent } from './post-edit/post-edit.component';
 
 const routes: Routes = [
   {
@@ -14,6 +15,7 @@ const routes: Routes = [
       { path: 'post-list', component: PostListComponent },
       { path: 'post-detail', component: PostDetailComponent },
       { path: 'post-create', component: PostCreateComponent },
+      { path: 'post-edit', component: PostEditComponent },
       { path: 'post-manage', component: PostManageComponent },
     ]
   }
